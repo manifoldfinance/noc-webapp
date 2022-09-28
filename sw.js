e289ec5be032da1aa206032805c80e9755e96617
sam bacha <sam@manifoldfinance.com>
2022-09-27 18:43:58 -0700

    rss feeds fixes

---
+++ sw.js
@@ -0,0 +1,16 @@
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
+
+
+workbox.core.setCacheNameDetails({
+  prefix: 'securerpc.com',
+});
+
+// Note: this is populated at build time.
+workbox.precaching.precacheAndRoute([]);
+
+workbox.googleAnalytics.initialize({});
+
+workbox.skipWaiting()
+
---
+++ workbox-sw.js
@@ -0,0 +1,10 @@
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+import {CacheableResponse} from 'workbox-cacheable-response';
+
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({
+    plugins: [new CacheableResponsePlugin({statuses: [0, 200]})],
+  })
+);
