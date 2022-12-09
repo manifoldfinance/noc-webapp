cd9641dd0e5773fe09cd660c26fc5a41f79f18ab
sam bacha <sam@manifoldfinance.com>
2022-12-09 01:48:26 -0800

    chore(repo): lockfile update

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash -x
 rm -rf dist
+mkdir -p public/.well-known
+mkdir -p dist/.well-known
+date "+%Y-%m-%dT%H:%M:%S%z" > dist/build_id.txt
+
 npm run build
+
+echo "Deploying to netlify..."
 netlify deploy --prod
+sleep 1
+exit 0
